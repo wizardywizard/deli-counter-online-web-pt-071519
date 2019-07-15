fb8bb23cac5f2ba5757ac6c8c66a2b8cd101dcdc
David Little <littledavid175@gmail.com>
2019-07-15 23:57:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(line, name)
   line.push(name)
-    puts "Welcome, #{name}. You are number #{0 + 1} in line."
+    puts "Welcome, #{name}. You are number #{line[-1] + 1} in line."
 end
 
 
