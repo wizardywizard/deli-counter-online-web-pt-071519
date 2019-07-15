3a9c1fe4d8493c93e088c10667afdd56c4a68915
David Little <littledavid175@gmail.com>
2019-07-15 22:50:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(names)
   if names.length > 0
-    names.each do |person, num|
+    names.each_with_index do |person, num|
       puts "The line is currently: #{num + 1}. #{person}"
   else
     puts "The line is currently empty."
