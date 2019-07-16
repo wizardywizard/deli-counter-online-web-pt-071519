8144e1a4275b8c32dff0e5d3b42e762fe682d20f
David Little <littledavid175@gmail.com>
2019-07-16 00:27:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 
 def now_serving(line)
   if line.length > 0
-      "currently serving #{line.shift}."
+     puts "currently serving #{line.shift}."
   else  
      puts "There is nobody waiting to be served!"
   end 
